5063c1744e1297146a06c6f25ee70c137f238374
A01367330 <42146822+A01367330@users.noreply.github.com>
2018-08-07 00:27:54 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -5,3 +5,24 @@ If everything went according to plan, the button will turn red and say SUBMIT FI
 function hello() {
 	return "hello edabit.com";
 }
+
+"""
+Create a function that takes two numbers as arguments and return their sum.
+
+"""
+function addition(a, b) {
+	 return a+b ;
+}
+"""
+Create a function that accepts an array and returns the last item in the array.
+Examples
+[1, 2, 3] ➞ 3
+
+['cat', 'dog', 'duck'] ➞ 'duck'
+
+[true, false, true] ➞ true
+"""
+function getLastItem(arr) {
+	 return arr[arr.length - 1]
+}
+
